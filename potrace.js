cd7073f2a5383ed19d5c64684fe09a151d42ce8c
Yoav Ilan <ilan.yoav@gmail.com>
2024-02-20 20:57:44 -0600

    somewhat functional fine tuning, better comments on code

--- potrace.js
+++ potrace.js
@@ -1303,21 +1303,28 @@ var Potrace = (function() {
   };
 })();
 
+// Start of our code
+
 const { loadImageFromUrl, process, getSVG, setParameter } = Potrace;
 
 let loadedOpenCV = false
 
+// openCV URL
 const openCvURL = "https://docs.opencv.org/4.7.0/opencv.js"
 
+// Load OpenCV on window load
 window.onload = function() {
   loadOpenCV(function () {
     const submitBtn = document.getElementById('fileSubmit')
     submitBtn.disabled = false;
     submitBtn.value = "Convert Image";
+    // initial smoothness parameter
+    setParameter({alphamax: 0})
   })
-  setParameter({alphamax: 5});
+
 }
 
+// Load OpenCV function called on window load
 function loadOpenCV(onComplete) {
     if (loadedOpenCV) {
         onComplete()
@@ -1335,6 +1342,12 @@ function loadOpenCV(onComplete) {
     }
 }
 
+// global url of jscanified image
+var blobURL = null;
+
+// function to get and download svg
+// not currently used
+// TODO: update to download svg on download button click
 function get_and_download_svg() {
   const svg = getSVG(1);
   const blob = new Blob([svg], { type: 'image/svg+xml' });
@@ -1353,6 +1366,7 @@ function get_and_download_svg() {
   document.body.removeChild(downloadLink);
 }
 
+// function to get svg and display it
 function get_svg() {
   const svg = getSVG(1);
   const blob = new Blob([svg], { type: 'image/svg+xml' });
@@ -1362,14 +1376,16 @@ function get_svg() {
   document.getElementById('myRange').disabled = false;
 }
 
+// jscanify object
 const scanner = new jscanify()
 
+// function to handle convert image button
 function handleFileUpload(event) {
+  // prevent default form submission
   event.preventDefault();
-  console.log('handleFileUpload')
+  // get uploaded file
   const file = document.getElementById('myFile').files[0];
   if(file) {
-    console.log('file found') 
     // Show the progress bar and initialize progress to 0
     const progressBar = document.getElementById('upload-progress-bar');
     progressBar.style.width = '0%';
@@ -1385,15 +1401,17 @@ function handleFileUpload(event) {
       if (progress >= 100) clearInterval(progressInterval);
     }, 100);
 
+    // Create a URL for the image in the file and load into image element
     const imageUrl = URL.createObjectURL(file);
     const newImg = document.createElement("img")
     newImg.src = imageUrl
-
+    // once loaded, jscanfiy and load into Potrace
     newImg.onload = function(){
       const resultCanvas = scanner.extractPaper(newImg, 1159.09090909, 1500);
+      // canvas to blob allows for Potrace to process the image
       resultCanvas.toBlob(function(blob) {
         // Create a URL for the Blob
-        var blobURL = URL.createObjectURL(blob);
+        blobURL = URL.createObjectURL(blob);
         // Pass the URL to Potrace LoadImageFromUrl
         loadImageFromUrl(blobURL);
       });
@@ -1406,6 +1424,7 @@ function handleFileUpload(event) {
       document.getElementById('myFile').value = '';
     }
   }
+  // If no file is uploaded and convert is clicked
   else {
     // Create a toast notification
     let toast = document.createElement("div");
@@ -1423,15 +1442,12 @@ function handleFileUpload(event) {
   }
 }
 
-// Listen for changes in the file input
+// Listen for convert file button click
 const fileInput = document.getElementById("fileSubmit");
 fileInput.addEventListener("click", handleFileUpload);
 
-
-
 // Get the file input element
 const fileUpload = document.getElementById('myFile');
-
 
 // Add an event listener for when a file is selected
 fileUpload.addEventListener('change', function(event) {
@@ -1443,4 +1459,14 @@ fileUpload.addEventListener('change', function(event) {
   document.getElementById('outputImage').src = imageUrl; 
   // myRange disabled
   document.getElementById('myRange').disabled = true;
+});
+
+// slider event listener to update smoothness
+const slider = document.getElementById('myRange');
+slider.addEventListener('change', function() {
+  const value = slider.value / 1000;
+  console.log(value);
+  setParameter({alphamax: value});
+  loadImageFromUrl(blobURL);
+  process(get_svg);
 });
